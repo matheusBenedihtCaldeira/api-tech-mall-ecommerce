fafb201e48ff6ed7fab160246429caa1b59a3061
bene <matheusbenediht@gmail.com>
2023-12-05 17:33:15 -0300

    First commit

---
+++ src/main/java/com/techMall/api/ApiTechMallApplication.java
@@ -0,0 +1,13 @@
+package com.techMall.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTechMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTechMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techMall/api/ApiTechMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techMall.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTechMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
